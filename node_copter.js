5aea63c515c14299185ff49c483d56d09cf69993
Swift <theycallmeswift@gmail.com>
2013-06-29 14:23:43 -0700

    We can now control the copters

--- node_copter.js
+++ node_copter.js
@@ -7,23 +7,24 @@ var drones = [];
 
 var child = exec('sudo nmap -n -p 23 -T4 -oX out.xml 192.168.43.1/24',
   function (error, stdout, stderr) {
-  	
+    
     fs.readFile('out.xml', 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
       parseString(data, function (err, result) {
-  	    result.nmaprun.host.forEach(function(host) {
+        result.nmaprun.host.forEach(function(host) {
           if (host.address[1] && host.address[1].$.vendor == 'Parrot') {
             var ip = host.address[0].$.addr;
-            var client = drone.createClient(ip);
+            var client = drone.createClient({ip: ip});
+            console.log(ip)
             drones.push(client);
-  	      }
-  	    });
+          }
+        });
         drones.forEach(function(client) {
           console.log(client);
           client.takeoff();
-          client.after(2000,function() {client.land();});
+          client.after(10000,function() {client.land();});
         });
       });
     });
